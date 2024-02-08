b991a09bd18f06d9005ab66bb442e67e76b65ea3
Daniil10Sh <danik2022odessa@gmail.com>
2024-02-08 20:34:46 +0200

    add index and script on repositoriy

---
+++ script.js
@@ -0,0 +1,27 @@
+const age = 13;
+console.log (age);
+
+const name1 = "Daniil";
+console.log (name1) ;
+
+const isStudent = "true";
+console.log (isStudemnt);
+
+const myString = "Спочатку мрії здаються неможливими, потім неправдоподібними, а потім неминучими. Крістофер Рів";
+console.log (myString);
+
+const myNumber = 23;
+const myNumber1 = 10;
+console.log (myNumber + myNumber1);
+
+const myNull = "null";
+console.log (myNull);
+
+const modalwindow = "Your name";
+console.log (prompt(modalwindow));
+
+const ok = "Confirm";
+console.log (confirm(ok));
+
+const alert1 = "Warning";
+console.log (alert(alert1))
